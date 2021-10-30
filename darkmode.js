29be277ea194873abefb6119162158e6978140d4
Emmi/Æddie <70488539+edbart@users.noreply.github.com>
2021-10-30 15:38:49 -0500

    more or less done

---
+++ darkmode.js
@@ -0,0 +1,64 @@
+'use strict';
+setTimeout(function(){
+window.themeBtn = document.getElementById('themeBtn');
+window.themeCookie = undefined;
+try {themeCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('darkmode=')).split('=')[1];}
+catch {console.log('no theme cookie');}
+let themeSheet = document.createElement('style');
+themeSheet.id='theme';
+document.head.appendChild(themeSheet);
+
+if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) && (themeCookie == undefined)) { /*browser dark, cookie none*/
+    themeBtn.textContent = 'Go Light!';
+}
+
+const goLight = function() {
+    console.log(themeSheet.sheet)
+    for (let x = 0; themeSheet.sheet.cssRules.length > 0; x++) {
+        themeSheet.sheet.deleteRule(0);
+    }
+    let style1 = 'body{background-color:#fff;color:#222;}';
+    let style2 = '.line{background-color:#222;}';
+    themeSheet.sheet.insertRule(style1,0);
+    themeSheet.sheet.insertRule(style2,1);
+    document.cookie = 'darkmode=0;max-age=31536000';
+    themeBtn.textContent = 'Go Dark!';
+}
+
+const goDark = function() {
+    console.log('dark')
+    for (let x = 0; themeSheet.sheet.cssRules.length > 0; x++) {
+        themeSheet.sheet.deleteRule(0);
+    }
+    let style1 = 'body{background-color:#222;color:#ddd;}';
+    let style2 = '.line{background-color:#ddd;}';
+    themeSheet.sheet.insertRule(style1,0);
+    themeSheet.sheet.insertRule(style2,1);
+    document.cookie = 'darkmode=1;max-age=31536000';
+    themeBtn.textContent = 'Go Light!';
+}
+
+if (themeCookie == '0') { /*cookie light*/
+    goLight();
+}
+
+if (themeCookie == '1') { /*cookie dark*/
+    themeBtn.textContent = 'Go Light!';
+    goDark();
+}
+
+const handler = (function() {
+    if (themeBtn.textContent.includes('Dark')) {
+        goDark();
+    }
+
+    else { if (themeBtn.textContent.includes('Light')) {
+        goLight();
+    }
+    else {
+        alert('Error! Please reload page.');
+    }}
+});
+themeBtn.addEventListener('click',handler);
+
+},10);
--- main.js
+++ main.js
@@ -1,1 +1,106 @@
-'use strict';
+'use strict';setTimeout(function(){
+const squares = ({
+'sq1': document.getElementById('1'),
+'sq2': document.getElementById('2'),
+'sq3': document.getElementById('3'),
+'sq4': document.getElementById('4'),
+'sq5': document.getElementById('5'), /*gets boxes*/
+'sq6': document.getElementById('6'),
+'sq7': document.getElementById('7'),
+'sq8': document.getElementById('8'),
+'sq9': document.getElementById('9')
+}),
+
+pointer1=(function(){clickHandle(1)}),
+pointer2=(function(){clickHandle(2)}),
+pointer3=(function(){clickHandle(3)}),
+pointer4=(function(){clickHandle(4)}),
+pointer5=(function(){clickHandle(5)}), /*click ponters*/
+pointer6=(function(){clickHandle(6)}),
+pointer7=(function(){clickHandle(7)}),
+pointer8=(function(){clickHandle(8)}),
+pointer9=(function(){clickHandle(9)});
+
+squares.sq1.addEventListener('click',pointer1);
+squares.sq2.addEventListener('click',pointer2);
+squares.sq3.addEventListener('click',pointer3);
+squares.sq4.addEventListener('click',pointer4);
+squares.sq5.addEventListener('click',pointer5); /*event listeners*/
+squares.sq6.addEventListener('click',pointer6);
+squares.sq7.addEventListener('click',pointer7);
+squares.sq8.addEventListener('click',pointer8);
+squares.sq9.addEventListener('click',pointer9);
+document.getElementById('oBtn').addEventListener('click',function(){
+    xFirstFlg = false;
+    document.getElementById('oBtn').parentElement.hidden = true;
+    document.getElementById('status').textContent = '\"O\" is going first. May the best player win!';
+});
+
+let xFirstFlg = true,
+turnNum = 0,
+board = [
+    undefined,
+    undefined,
+    undefined,
+    undefined,  /*game vars*/
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined
+];
+
+let markParser = function() {
+    let letter = '';
+    if (xFirstFlg) {
+        if (turnNum%2 == 0) {
+            letter = 'X';
+        }
+        else {
+            letter = 'O';
+        }
+    }
+    else {
+        if (turnNum%2 == 0) {
+            letter = 'O';
+        }
+        else {
+            letter = 'X';
+        }
+    }
+    return letter;
+}
+
+let clickHandle = function(boxNum) {
+    document.getElementById('oBtn').parentElement.hidden = true;
+    let mark = markParser();
+    squares['sq'+boxNum.toString()].querySelector('span').textContent = mark;
+    board[boxNum] = turnNum%2;
+    checkForWin();
+    turnNum++;
+}
+
+let checkForWin = function() {
+    let sqCheck = null;
+    if ((board[1]==board[2])&&(board[2]==board[3])&&(board[1]!==undefined)) {sqCheck=1;}
+    if ((board[4]==board[5])&&(board[5]==board[6])&&(board[4]!==undefined)) {sqCheck=4;}
+    if ((board[7]==board[8])&&(board[8]==board[9])&&(board[7]!==undefined)) {sqCheck=7;}
+    if ((board[1]==board[4])&&(board[4]==board[7])&&(board[1]!==undefined)) {sqCheck=1;}
+    if ((board[2]==board[5])&&(board[5]==board[8])&&(board[2]!==undefined)) {sqCheck=2;}
+    if ((board[3]==board[6])&&(board[6]==board[9])&&(board[3]!==undefined)) {sqCheck=3;}
+    if ((board[1]==board[5])&&(board[5]==board[9])&&(board[1]!==undefined)) {sqCheck=1;}
+    if ((board[3]==board[5])&&(board[5]==board[7])&&(board[3]!==undefined)) {sqCheck=3;}
+    if (sqCheck!==null) {
+        let mark = squares['sq'+sqCheck.toString()].querySelector('span').textContent;
+        document.querySelector('#winDiv h2').textContent = '\"'+mark+'\" is the Winner!';
+        document.getElementById('playAgain').hidden = false;
+    }
+    else {
+        if (turnNum >= 8) {
+            document.querySelector('#winDiv h2').textContent = 'The game is a tie.';
+            document.getElementById('playAgain').hidden = false;
+        }
+    }
+}
+},20);
